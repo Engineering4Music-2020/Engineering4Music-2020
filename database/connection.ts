e93f3d4cb2783486fa7686fb524bc94a5e62ef3f
Mitschelor <michael@schnyder.cc>
2020-09-07 09:45:08 +0200

    Added function to automatically add data to the DB

--- database/connection.ts
+++ database/connection.ts
@@ -3,23 +3,37 @@ import dotenv from 'dotenv';
 import { getData } from '../sensors/src/main';
 
 
-const temp = getData().then((temp) => {
-    return temp.toString()
-})
+const temp = getData().then((data) => {
+    let humidity = data.humidity;
+    let temperature = data.temperature;
+    let date = data.date;
 
-
-dotenv.config();
-
-const client = new Client({
-    connectionString: process.env.DB_URI,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
-
-;(async () => {
-  await client.connect();
-  const res = await client.query(`INSERT INTO test VALUES('${(await temp).toString()}');`);
-  console.log(res.rows) // Hello world!
-  await client.end()
-})()
+    dotenv.config();
+    
+    async function fillDataBase() {
+        const client = new Client({
+            connectionString: process.env.DB_URI,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+        
+        let d = new Date();
+        let year = d.getFullYear();
+        let month = d.getMonth() + 1;
+        let day = d.getDate();
+        let hour = d.getHours();
+        let minutes = d.getMinutes();
+        console.log(`date '${year}-${month}-${day}' + time '${hour}:${minutes}'`);
+            try {
+                await client.connect();
+                const res = await client.query(`INSERT INTO data VALUES(${humidity}, ${temperature}, 1009, date '${year}-${month}-${day}' + time '${hour}:${minutes}');`);
+                console.log(res.rows);
+            } catch(error) {
+                console.log(error);
+            } finally {
+                client.end();
+            }
+        }   
+        fillDataBase();
+});
