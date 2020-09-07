73716bae32802572cf908333b8ca405216c343ed
larscarl <68387078+larscarl@users.noreply.github.com>
2020-09-07 10:10:53 +0200

    revert changes in measureData.ts, because they are essential for the current website

--- sensors/src/measureData.ts
+++ sensors/src/measureData.ts
@@ -2,8 +2,11 @@ export class MeasureData {
 	public temperature: number;
 	public humidity: number;
 
+	public date: Date;
+
 	constructor(temperature: number, humidity: number) {
 		this.temperature = temperature;
 		this.humidity = humidity;
+		this.date = new Date();
 	}
 }
