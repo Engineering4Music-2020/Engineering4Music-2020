a0c7c8895af2fa59ec4c2aaa81b10252131e947f
larscarl <68387078+larscarl@users.noreply.github.com>
2020-09-10 17:37:38 +0200

    add about-page + controller

--- web/src/app.ts
+++ web/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import exphbs from "express-handlebars";
 
+import * as aboutController from "./controllers/about";
 import * as homeController from "./controllers/home";
 import * as dataOutputController from "./controllers/dataOutput";
 import * as downloadDataController from "./controllers/downloadData";
@@ -20,6 +21,7 @@ app.set("view engine", "handlebars");
 
 // Primary app routes.
 
+app.get("/about", aboutController.index);
 app.get("/home", homeController.index);
 app.get("/main", dataOutputController.main);
 app.get("/data", downloadDataController.loadData);
