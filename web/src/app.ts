5a86d18bf15e0da524a4a69324d75d740708b3fd
larscarl <68387078+larscarl@users.noreply.github.com>
2020-09-17 10:20:16 +0200

    refactoring routings and databank requests

--- web/src/app.ts
+++ web/src/app.ts
@@ -1,20 +1,22 @@
-import express, { Request, Response} from "express";
+import express, { Request, Response } from "express";
 import path from "path";
 import exphbs from "express-handlebars";
 import session from "express-session";
 import bodyParser from "body-parser";
 
-
 import * as aboutController from "./controllers/about";
 import * as homeController from "./controllers/home";
+import * as dataController from "./controllers/data";
 import * as dataOutputController from "./controllers/dataOutput";
 import * as downloadDataController from "./controllers/downloadData";
 import * as registerController from "./controllers/register";
 
 // Create Express server
+
 const app = express();
 
 // Express configuration
+
 const public_path = path.join(__dirname, "../public");
 console.log("Public path is " + public_path);
 app.use(express.static(public_path));
@@ -26,13 +28,16 @@ app.set("view engine", "handlebars");
 // Primary app routes.
 
 app.get("/", registerController.form);
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.post("/auth", registerController.register);
 app.get("/about", aboutController.index);
 app.get("/home", homeController.index);
 app.get("/main", dataOutputController.main);
-app.get("/data", downloadDataController.loadData);
+app.get("/data", dataController.index);
+// app.get("/data", downloadDataController.loadData);
+
+// Databank Queries
 
 app.get("/dataJSONAll", downloadDataController.loadJSONAll);
 app.get("/dataJSONlast24h", downloadDataController.loadJSONlast24h);
--- web/src/controllers/downloadData.ts
+++ web/src/controllers/downloadData.ts
@@ -4,7 +4,11 @@ import { Request, Response } from "express";
 
 dotenv.config();
 
-async function readData(res: Response) {
+async function createNewClientAndConnectToDatabase(
+	req: Request,
+	res: Response,
+	query: string
+) {
 	const client = new Client({
 		connectionString: process.env.DB_URI,
 		ssl: {
@@ -16,12 +20,8 @@ async function readData(res: Response) {
 		console.log(`Connecting`);
 		await client.connect();
 		console.log(`Connected to database`);
-		const result = await client.query(`SELECT * FROM data ORDER BY date;`);
-		console.log(result);
-		res.render("data", {
-			layout: false,
-			data: result.rows,
-		});
+		const result = await client.query(query);
+		res.send(JSON.stringify(result));
 	} catch (error) {
 		console.log(error);
 	} finally {
@@ -29,98 +29,21 @@ async function readData(res: Response) {
 		console.log(`Disconnected from Database`);
 	}
 }
-
-export const loadData = (req: Request, res: Response): void => {
-	readData(res);
-};
 
 export async function loadJSONAll(req: Request, res: Response) {
-	const client = new Client({
-		connectionString: process.env.DB_URI,
-		ssl: {
-			rejectUnauthorized: false,
-		},
-	});
-
-	try {
-		console.log(`Connecting`);
-		await client.connect();
-		console.log(`Connected to database`);
-		const result = await client.query(`SELECT * FROM data ORDER BY date;`);
-		res.send(JSON.stringify(result));
-	} catch (error) {
-		console.log(error);
-	} finally {
-		await client.end();
-		console.log(`Disconnected from Database`);
-	}
+	const query = `SELECT * FROM data ORDER BY date;`;
+	createNewClientAndConnectToDatabase(req, res, query);
 }
-export async function loadJSONlast24h(req: Request, res: Response) {
-	const client = new Client({
-		connectionString: process.env.DB_URI,
-		ssl: {
-			rejectUnauthorized: false,
-		},
-	});
 
-	try {
-		console.log(`Connecting`);
-		await client.connect();
-		console.log(`Connected to database`);
-		const result = await client.query(
-			`SELECT * FROM data WHERE date BETWEEN NOW() - INTERVAL '24 HOURS' AND NOW() ORDER BY date;`
-		);
-		res.send(JSON.stringify(result));
-	} catch (error) {
-		console.log(error);
-	} finally {
-		await client.end();
-		console.log(`Disconnected from Database`);
-	}
+export async function loadJSONlast24h(req: Request, res: Response) {
+	const query = `SELECT * FROM data WHERE date BETWEEN NOW() - INTERVAL '24 HOURS' AND NOW() ORDER BY date;`;
+	createNewClientAndConnectToDatabase(req, res, query);
 }
 export async function loadJSONlast7d(req: Request, res: Response) {
-	const client = new Client({
-		connectionString: process.env.DB_URI,
-		ssl: {
-			rejectUnauthorized: false,
-		},
-	});
-
-	try {
-		console.log(`Connecting`);
-		await client.connect();
-		console.log(`Connected to database`);
-		const result = await client.query(
-			`SELECT * FROM data WHERE date BETWEEN NOW() - INTERVAL '7 DAYS' AND NOW() ORDER BY date;`
-		);
-		res.send(JSON.stringify(result));
-	} catch (error) {
-		console.log(error);
-	} finally {
-		await client.end();
-		console.log(`Disconnected from Database`);
-	}
+	const query = `SELECT * FROM data WHERE date BETWEEN NOW() - INTERVAL '7 DAYS' AND NOW() ORDER BY date;`;
+	createNewClientAndConnectToDatabase(req, res, query);
 }
 export async function loadJSONlast1m(req: Request, res: Response) {
-	const client = new Client({
-		connectionString: process.env.DB_URI,
-		ssl: {
-			rejectUnauthorized: false,
-		},
-	});
-
-	try {
-		console.log(`Connecting`);
-		await client.connect();
-		console.log(`Connected to database`);
-		const result = await client.query(
-			`SELECT * FROM data WHERE date BETWEEN NOW() - INTERVAL '1 MONTH' AND NOW() ORDER BY date;`
-		);
-		res.send(JSON.stringify(result));
-	} catch (error) {
-		console.log(error);
-	} finally {
-		await client.end();
-		console.log(`Disconnected from Database`);
-	}
+	const query = `SELECT * FROM data WHERE date BETWEEN NOW() - INTERVAL '1 MONTH' AND NOW() ORDER BY date;`;
+	createNewClientAndConnectToDatabase(req, res, query);
 }
