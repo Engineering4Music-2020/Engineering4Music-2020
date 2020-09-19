16d18e94516d357eb9e5ff52ba7155c190e3e954
usrt0022 <usrt0022@students.zhaw.ch>
2020-09-19 14:11:04 +0200

    Added sendMail Function

---
+++ auto-mail/src/mailsender.ts
@@ -0,0 +1,25 @@
+import sgMail from "@sendgrid/mail";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const apiKey = process.env.SENDGRID;
+
+sgMail.setApiKey(apiKey);
+
+export const sendMail = () => {
+    const mg = {
+        to: 'michael.schnyder.immer@gmail.com',
+    from: 'michael@schnyder.cc',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+
+sgMail.send(msg).then(() => {
+    console.log("Success");
+})
+.catch((error:any) => {
+    console.log(error);
+});
+}
