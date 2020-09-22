cab4386f23d4e7db96e6538a8fff4ed8eec22210
larscarl <68387078+larscarl@users.noreply.github.com>
2020-09-22 13:05:53 +0200

    imports mailsender.ts

--- auto-mail/src/mailsender.ts
+++ auto-mail/src/mailsender.ts
@@ -1,29 +1,39 @@
 import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
-import { temperature_threshold, humidity_threshold } from "../../database/src/warnUser"
+import {
+	temperature_threshold,
+	humidity_threshold,
+} from "../../database/src/warnUser";
 
 dotenv.config();
 
 const apiKey: any = process.env.API_KEY;
 
-export const sendMail = (type: string, value: number, subject: string, email: string) {
-    sgMail.setApiKey(apiKey);
+export const sendMail = (
+	type: string,
+	value: number,
+	subject: string,
+	email: string
+) => {
+	sgMail.setApiKey(apiKey);
 
-    const msg = {
-        to: email,
-        from: `michael@schnyder.cc`,
-        subject: `${type} is ${subject}.`,
-        // text: `and easy to do anywhere, even with Node.js`,
-        html: `The ${type} in your room is ${value}. The optimal range for temperature is between ${temperature_threshold[0]} and ${temperature_threshold[1]}. The optimal range for humidity is between ${humidity_threshold[0]} and ${humidity_threshold[1]}.`,
-    };
+	const msg = {
+		to: email,
+		from: `michael@schnyder.cc`,
+		subject: `${type} is ${subject}.`,
+		// text: `and easy to do anywhere, even with Node.js`,
+		html: `The ${type} in your room is ${value}. The optimal range for temperature is between ${temperature_threshold[0]} and ${temperature_threshold[1]}. The optimal range for humidity is between ${humidity_threshold[0]} and ${humidity_threshold[1]}.`,
+	};
 
-    sgMail.send(msg).then(() => {
-        console.log(`Mail with subject ${msg.subject} has been sent.`);
-    })
-        .catch((error: any) => {
-            console.log(error);
-        });
-}
+	sgMail
+		.send(msg)
+		.then(() => {
+			console.log(`Mail with subject ${msg.subject} has been sent.`);
+		})
+		.catch((error: any) => {
+			console.log(error);
+		});
+};
 
 // export const sendWarning = (temperature: number, humidity: number, email: string) => {
 
