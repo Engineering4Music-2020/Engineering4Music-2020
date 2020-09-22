c1c005cf574c4840a9d627e75bd0a68f9299da83
Michael Schnyder <michael@schnyder.cc>
2020-09-22 22:09:43 +0200

    deleted js files

--- web/src/controllers/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = function (req, res) {
-    res.render("data", {
-        layout: false,
-        title: "Data",
-    });
-};
--- web/src/controllers/home.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = function (req, res, next) {
-    res.render("home", {
-        layout: false,
-        title: "Home",
-        login: req.isAuthenticated(),
-    });
-};
