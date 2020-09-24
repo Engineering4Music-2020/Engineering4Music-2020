b7a15eca61f6a775bf65032836d6c2dc1a8b9b13
Mitschelor <michael@schnyder.cc>
2020-09-24 14:25:54 +0200

    Fixed wrong query

--- web/src/controllers/register.ts
+++ web/src/controllers/register.ts
@@ -42,7 +42,7 @@ const uploadToDB = (
 				}
 			} else {
 				const result = await client.query(
-					`SELECT * FROM login WHERE raspiid = ${raspiId}`
+					`SELECT * FROM login WHERE id = ${raspiId}`
 				);
 				if (result.rows[0]) {
 					req.flash("err_msg", "This ID already belongs to someone else!");
